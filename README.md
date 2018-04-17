bdf166556f0412fa096087381f4fc451165383df
Benjamin <benjamin@simpulse.nl>
2018-04-17 17:17:48 +0200

    Updated filters

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
 # python-combine-csv
-Combine multiple .csv files with the same structure into a single .csv file, whilst optionally filtering specific location IDs.
+Combine multiple .csv files with the same structure into a single .csv file, whilst optionally filtering on specific columns.
 
 ## 0. Usage
+```bash
+python3 combine_csv.py --data [--output] [--filter]
 ```
-python3 combine_csv.py --folder "path/to/folder" [--output "output/file.csv"] [--id "id1,id2,id3"]
-```
-Where '--folder' is a relative path to a folder containing the .csv files, '--output' is a relative path to a .csv file, and '--id' is a comma separated string with location IDs.
+
+**data**
+
+1. Comma separated list of .csv files
+2. Path to folder with trailing '/' that contains .csv files
+
+**output**
+
+Path to the output file. Defaults to 'combined_files.csv' in current folder.
+
+**filter**
+
+Comma separated list of filters.
+
+Possible comparisons:
+
+* column_name __IN__ "list|of|things"
+* column_name __>__ 300
+* column_name __>=__ 100
+* column_name __<__ 200
+* column_name __<=__ 800
+* column_name __==__ "thing"
+* column_name __!=__ "thing"
--- combine_csv.py
+++ combine_csv.py
@@ -2,14 +2,20 @@
 import time
 import glob
 import argparse
-import os
+import re
+import operator
+import sys
 
 # parse options given to script
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--folder", help="The folder that contains the to be combined .csv files", metavar="", default="")
+parser.add_argument("-d", "--data", help="The folder that contains the to be combined .csv files (with trailing '/')", metavar="", default="")
 parser.add_argument("-o", "--output", help="The output file to be generated, defaults to 'combined_files.csv'", metavar="", default="combined_files.csv")
-parser.add_argument("-i", "--id", help="ID's to filter the data on (comma separated)", metavar="", default="")
+parser.add_argument("-f", "--filter", help="Filters applied to the rows in the source files (comma seperated)", metavar="", default="")
 options = parser.parse_args()
+
+input_datafile = options.data
+input_outputfile = options.output
+input_filter = options.filter
 
 # start timer
 timer = time.time()
@@ -17,14 +23,68 @@ timer = time.time()
 # output that we're doing something
 print("\nPreparing everything... (The script can be aborted by pressing ctrl+c)")
 
+# split the given filters and put them in a list
+input_filters = input_filter.split(",") if input_filter != "" else []
+
+# create a new empty list
+filters = []
+
+# operator lookup
+operators = {
+    ">": operator.gt,
+    ">=": operator.ge,
+    "<": operator.lt,
+    "<=": operator.le,
+    "=": operator.eq,
+    "==": operator.eq,
+    "!=": operator.ne,
+    "<>": operator.ne,
+    "IN": operator.contains
+}
+
+# loop through all given filters
+for filter in input_filters:
+
+    # match them against a regular expression
+    match = re.fullmatch("\s?(\w+)\s?(>=?|<=?|==?|!=|<>|IN)\s?(?:\'|\")?(.*[^\'\"])?(?:\'|\")?\s?", filter)
+
+    # assume the value is not a number
+    number = False
+    value = match.group(3) or ""
+
+    # try to interpret it as a number
+    try:
+        value = int(value)
+    except ValueError:
+        pass
+    else:
+        number = True
+
+    # save the filter to the filter list
+    filters.append({
+        "column": match.group(1),
+        "operator": operators[match.group(2)],
+        "value": value,
+        "number": number
+    })
+
 # open a new file that we will fill
-combined = open(options.output, "w")
+combined = open(input_outputfile, "w")
 
 # init counter
 counter = 0
 
+# start with empty file list
+file_list = []
+
+# fill the list of data files
+if "," in input_datafile:
+    file_list.extend(input_datafile.split(","))
+elif options.data[-1:] == "/":
+    file_list.extend(glob.glob(input_datafile + "*.csv"))
+
 # list all files in directory
-for i, name in enumerate(glob.glob(options.folder + "/*.csv")):
+for i, name in enumerate(file_list):
 
     # update counter
     counter += 1
@@ -39,11 +99,47 @@ for i, name in enumerate(glob.glob(options.folder + "/*.csv")):
     # loop through lines
     for j, line in enumerate(f):
 
-        # check if id should be filtered
-        if (options.id != "" and (i != 0 or j != 0)):
+        # save the column names
+        if (i == 0 and j == 0):
+            columns = line.split(",")
 
-            if (line.split(",")[2] not in options.id.split(",")):
+        # check if the line should be filtered
+        if (len(filters) > 0 and (i != 0 or j!= 0)):
 
+            # save values from line in list
+            values = line.split(",")
+
+            # start with not filtering it
+            filter_row = False
+
+            # loop through filters
+            for k, filter in enumerate(filters):
+
+                value = values[columns.index(filter["column"])]
+
+                # if it should be a number and is not missing
+                if filter["number"] is True and value is not "":
+
+                    # try to interpret it as a number
+                    try:
+                        value = int(value)
+                    except ValueError:
+                        sys.exit("Error: Value in column appears to be not a number, which it should be.")
+
+                # change order of filter values for IN operator
+                if filter["operator"] == operator.contains:
+                    value1 = filter["value"]
+                    value2 = value
+                else:
+                    value1 = value
+                    value2 = filter["value"]
+
+                # filter the value
+                if (value is "" and filter["number"] is False or value is not "") and not filter["operator"](value1, value2):
+                    filter_row = True
+
+            # should the row be filtered, then go to next row
+            if filter_row == True:
                 continue
 
         # write the line to the end file
@@ -63,4 +159,4 @@ print("\rFiles combined: " + str(counter))
 
 # print duration of script execution
 print("Finished in " + str(round(time.time() - timer, 2)) + " seconds\n")
-os.system('say "Bliep"')
+print("\a")
